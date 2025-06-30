f6b7362c41894ed700b00371cc984a8339fb545f
ImBdang <dtc225200211@ictu.edu.vn>
2025-06-30 08:50:47 +0700

    update web

---
+++ web/main.js
@@ -0,0 +1,79 @@
+let toado = []
+
+function read_toado(){
+    return fetch('bounding_boxes.json')
+    .then(response => response.json())
+    .then(data => {
+        toado = data
+    })
+    .catch(error => console.error('Error loading JSON:', error));
+}
+
+function load_option(){
+    content = ``
+    for (let i = 0; i < toado.length; i++){
+        template = `<option value="${i}">Slot ${i}</option>\n`
+        content += template
+ 
+    }
+    document.getElementById("parking_slot").innerHTML = content
+}
+
+// function set_green(){
+//     const boundingBox = document.getElementById("bounding_box");
+//     boundingBox.style.display = "block";
+//     for (let slot = 0; slot < toado.length; slot++){
+//         boundingBox.style.width = `${toado[slot].w}px`;
+//         boundingBox.style.height = `${toado[slot].h}px`;
+//         boundingBox.style.left =`${toado[slot].x}px`;
+//         boundingBox.style.top = `${toado[slot].y}px`;
+//     }
+// }
+
+async function main(){
+    await read_toado()
+    load_option()
+    drawBoxesOnCanvas()
+}
+
+function drawBoxesOnCanvas() {
+  const canvas = document.getElementById('parkingCanvas');
+  const ctx = canvas.getContext('2d');
+  
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+  
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+
+  toado.forEach(box => {
+    ctx.strokeStyle = '#00FF00'
+    ctx.lineWidth = 2;
+    ctx.strokeRect(box.x, box.y, box.w, box.h);
+
+    ctx.fillStyle ='rgba(120, 247, 95, 0.5)';
+    ctx.fillRect(box.x, box.y, box.w, box.h);
+  });
+}
+
+
+function turn_red(slot){
+    const canvas = document.getElementById('parkingCanvas');
+    const ctx = canvas.getContext('2d');
+    box = toado[slot]
+
+    ctx.clearRect(box.x, box.y, box.w , box.h );
+
+    ctx.strokeStyle = '#FF0000'
+    ctx.lineWidth = 2;
+    ctx.strokeRect(box.x, box.y, box.w, box.h);
+    ctx.fillStyle ='rgba(204, 32, 32, 0.5)';
+    ctx.fillRect(box.x, box.y, box.w, box.h);
+}
+
+document.getElementById("book_btn").addEventListener("click", function() {
+    const slot = document.getElementById("parking_slot").value;
+    turn_red(slot)
+});
+
+main()
